4b42233771ad1145417b0726f4e3331e4daf030b
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-09-07 10:19:29 +0530

    auth filter add

--- POS_Layerd_SPA/Back_End/src/lk/ijse/pos/filter/AuthFilter.java
+++ POS_Layerd_SPA/Back_End/src/lk/ijse/pos/filter/AuthFilter.java
@@ -1,2 +1,48 @@
-package lk.ijse.pos.filter;public class AuthFilter {
+package lk.ijse.pos.filter;
+
+import lk.ijse.pos.util.ResponseUtil;
+
+import javax.json.JsonObject;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = "/*")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("AUth filter Invoked");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+        System.out.println("Auth filter do filter Invoked");
+
+        String auth = request.getHeader("Auth");
+
+        if(request.getMethod().equals("OPTION")){
+            filterChain.doFilter(servletRequest, servletResponse);
+        }else{
+            if (auth != null && auth.equals("user=admin,pass=admin")){
+                filterChain.doFilter(servletRequest,servletResponse);
+            }else{
+                response.addHeader("Content-Type","application/json");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                JsonObject jsonObject = ResponseUtil.getJson("Auth-Error","You are not Authentic to use this Service.!");
+                response.getWriter().print(jsonObject);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Auth filter destroy");
+    }
 }
--- POS_Layerd_SPA/Back_End/src/lk/ijse/pos/util/ResponseUtil.java
+++ POS_Layerd_SPA/Back_End/src/lk/ijse/pos/util/ResponseUtil.java
@@ -1,4 +1,21 @@
 package lk.ijse.pos.util;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
 public class ResponseUtil {
+    public static JsonObject getJson(String state, String message, JsonArray...data){
+        JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder();
+        jsonObjectBuilder.add("state",state);
+        jsonObjectBuilder.add("message", message);
+
+        if (data.length>0){
+            jsonObjectBuilder.add("data",data[0]);
+        }else {
+            jsonObjectBuilder.add("data","");
+        }
+        return jsonObjectBuilder.build();
+    }
 }
