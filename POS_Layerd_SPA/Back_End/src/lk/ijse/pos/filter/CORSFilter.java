ff16168ea677e229ff897340d00c402e84be79ec
chathushka1 <www.chathushkamadumal123@gmail.com>
2023-09-07 10:41:19 +0530

    CORS Filter add

--- POS_Layerd_SPA/Back_End/src/lk/ijse/pos/filter/CORSFilter.java
+++ POS_Layerd_SPA/Back_End/src/lk/ijse/pos/filter/CORSFilter.java
@@ -7,21 +7,36 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/*")
-public class CROSFilter implements Filter {
+public class CORSFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        System.out.println("Cros Filter Invoked");
+        System.out.println("CORS Filter Invoked");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletRequest;
-        System.out.println("");
+        System.out.println("CORS Filter do Filter Invoked");
+
+        String method = request.getMethod();
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        if (method.equals("OPTIONS")){
+            response.addHeader("Access-Control-Allow-Origin","*");
+            response.addHeader("Access-Control-Allow-Methods","PUT, DELETE");
+            response.addHeader("Access-Control-Allow-Headers","content-type,auth");
+            filterChain.doFilter(servletRequest,servletResponse);
+        }else {
+            response.addHeader("Access-Control-Allow-Origin","*");
+            response.addHeader("Content-Type","application/json");
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
     }
 
     @Override
     public void destroy() {
+        System.out.println("CORS Filter Destroyed");
 
     }
 }
